101891d2fc3e04460e45ae631511b157b71cd220
unknown <John@DEUSEXMACHINA.lan>
2014-01-20 16:33:10 -0800

    Refactoring and renaming PWStatus model factory

--- components/src/main/java/info/jchein/pwstatus/model/PWStatusFactory.java
+++ components/src/main/java/info/jchein/pwstatus/model/PWStatusFactory.java
@@ -1,19 +1,44 @@
 package info.jchein.pwstatus.model;
 
-import java.util.ArrayList;
+import info.jchein.pwstatus.model.PasswordValidity.ResultKind;
 
-public final class PasswordModelFactory {
-	public static Password createPassword(final String contentString) {
-		return new Password(contentString);
+import java.util.List;
+
+public final class PWStatusFactory {
+	public static PasswordValidity createPassedPasswordValidityTest() {
+		return new PasswordValidity();
 	}
 	
-	public static PasswordValidity createPasswordValidity(ArrayList<String> errorsFound) {
+	public static PasswordValidity createIncompletePasswordValidityTest() {
 		PasswordValidity retVal = new PasswordValidity();
-		retVal.setValid(errorsFound==null);
+		retVal.setResult(ResultKind.UNKNOWN);
+		
+		return retVal;
+	}
+	
+	public static PasswordValidity createIncompletePasswordValidityTest(List<String> errorsFound) {
+		PasswordValidity retVal = new PasswordValidity();
+		if( errorsFound == null || errorsFound.size() == 0 ) {
+			retVal.setResult(ResultKind.UNKNOWN);
+		} else {
+			retVal.setResult(ResultKind.UNKNOWN_INVALID);
+			retVal.setErrors(errorsFound);
+		}
+		
+		return retVal;
+	}
+	
+	public static PasswordValidity createFailedPasswordValidityTest(List<String> errorsFound) {
+		if( errorsFound == null || errorsFound.size() == 0 ) {
+			throw new IllegalArgumentException();
+		}
+		
+		final PasswordValidity retVal = new PasswordValidity();
+		retVal.setResult(ResultKind.INVALID);
 		retVal.setErrors(errorsFound);
 		
 		return retVal;
 	}
 
-	private PasswordModelFactory() { }
+	private PWStatusFactory() { }
 }
